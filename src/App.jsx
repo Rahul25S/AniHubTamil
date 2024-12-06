b3986cdd5eb530907c0029d4cb534eec70303258
Rahul25S <rahul11022004@gmail.com>
2024-12-06 19:56:55 +0530

    created

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Profile from './Pages/Profile';
+import Navbar from './Components/Navbar';
+import { AuthProvider } from './context/AuthContext';  // Import the AuthProvider
+//import ProtectedRoute from './Components/ProtectedRoute';
+import StudioApartmentGoodLightingAngelIncluded from './Anime/StudioApartmentGoodLightingAngelIncluded';
+import TimeLoopTheVillainessEnjoys from './Anime/TimeLoopTheVillainessEnjoys';
+import Recovery from './Pages/Recovery';
+import Reset from './Pages/Reset';
+import PageNotFound from './Pages/PageNotFound';
+//<ProtectedRoute></ProtectedRoute>
+const App = () => {
+  return (
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />}/>
+        <Route path="/recovery" element={<Recovery />}/>
+        <Route path="/reset" element={<Reset />}/>
+        <Route path="/*" element={<PageNotFound />}/>
+        <Route path="/Studio-Apartment-Good-Lighting-Angel-Included" element={<StudioApartmentGoodLightingAngelIncluded />} />
+        <Route path="/7th-Time-Loop-The-Villainess-Enjoys-a-Carefree-Life-Married-to-Her-Worst-Enemy" element={<TimeLoopTheVillainessEnjoys />} />
+      </Routes>
+    </AuthProvider>
+  );
+};
+
+export default App;
---
+++ src/Pages/Helper/helper.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+axios.defaults.baseURL=process.env.REACT_APP_SERVER_DOMAIN
+//make api request
+
+//authenticate function
+export async function authenticate(username){
+    try{
+        return await axios.post('api/authenticate',{username})
+    } catch(error){
+        return{error:"Username doesn't exist...!"}
+    }
+}
+//get user details
+export async function getUser({username}){
+    try{
+const{data}=await axios.get(`/api/user/${username}`)
+return{data}
+    }catch(error){
+return{error:"Password doesn't Match...!"}
+    }
+}
+//register user function
+export async function registerUser(credentials){
+    try{
+        const{data:{msg},status}=await axios.post(`/api/register`,credentials)
+        let{username,email}=credentials
+        //send email
+        if(status==201){
+            await axios.post('/api/registerMail',{username,userEmail:email,text:msg})
+        }
+        return Promise.resolve(msg)
+    }catch(error){
+        return MdPanoramaFishEye.reject({error})
+    }
+}
+//login function
+export async function verifyPassword({username,password}){
+    try{
+        if(username){
+            const{data}=await axios.post('/api/login',{username,password})
+            return Promise.resolve({data})
+        }
+    }catch(error){
+        return Promise.reject({error:"Password doesn't Match...!"})
+    }
+}
